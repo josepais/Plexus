f84045c3c317427622a6ea72e8d7fd15c6d7953d
joanpenyatorres <joan.penyatorres@plexus.es>
2022-04-21 10:48:37 +0200

    Comentario de prueba para repo

--- ApiBonitaUIPath/DarAltaEmpleado.cs
+++ ApiBonitaUIPath/DarAltaEmpleado.cs
@@ -53,7 +53,7 @@ namespace ApiBonitaUIPath {
         public string Reference { get; set; }
         public string Priority { get; set; }
     }
-
+    //Comentario
     public class Root {
         public ItemData itemData { get; set; }
     }
